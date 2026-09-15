7cf52041d14b402852c2c7fac148f33a7e130808
ddong8 <1413470138@qq.com>
2026-09-15 22:39:05 +0800

    feat(linux): 支持 Linux 桌面端 Flutter 客户端并在 GitHub Actions 接入 Release 自动构建

--- mobile/lib/core/services/update_service.dart
+++ mobile/lib/core/services/update_service.dart
@@ -7,7 +7,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../storage.dart';
 
 /// Current application version
-const String kAppCurrentVersion = '1.0.3';
+const String kAppCurrentVersion = '1.0.4';
 
 class UpdateInfo {
   final String version;
