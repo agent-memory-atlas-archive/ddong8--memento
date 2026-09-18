764c33a3fcc78437214b1832f6c80f619448b688
ddong8 <1413470138@qq.com>
2026-09-19 01:17:57 +0800

    chore(release): bump version to v1.0.18

--- mobile/lib/core/services/update_service.dart
+++ mobile/lib/core/services/update_service.dart
@@ -7,7 +7,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../storage.dart';
 
 /// Current application version — MUST match pubspec.yaml `version` on every release!
-const String kAppCurrentVersion = '1.0.17';
+const String kAppCurrentVersion = '1.0.18';
 
 class UpdateInfo {
   final String version;
